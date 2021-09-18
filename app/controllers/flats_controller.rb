a8e4ed957c19d816957c419ef034f38286d48a65
Andrea Nasi <nasi.andrea93@gmail.com>
2021-09-18 14:36:24 +0200

    Fixed a small problem with the Flat Controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -12,8 +12,11 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     @flat.user = current_user
-
-    @flat.save ? redirect_to flat_path(@flat) : render :new
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def show
