83c9ea239c0f19ea9ff5d1ce785a02e3de0f5ddd
Fabrizio Chiavari <fabrizio.chiavari@gmail.com>
2021-09-21 10:11:27 +0200

    Created Flats page and show flat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'Date'
+
+array = [User.all, Flat.all, Booking.all]
+puts "Remove all data from DB"
+
+array.each do |item|
+  item.destroy_all
+end
+
+puts "All data removed!"
+
+# new_user = User.create!(email: "email@test.com", password: "test123", owner: false, name: "Mario", lastname: "Rossi" )
+# new_flat = Flat.create!(title: "Loft Colosseo", address: "Via dei Fori Imperiali", description: "Amazing flat close to the city center of Rome!", city: "Rome", price: 200, size: 6, user_id: new_user.id)
+# new_booking = Booking.create!(check_in: Date.parse("Sat, 24 May 2020").strftime('%Y-%m-%d'), check_out:Date.parse("Sat, 26 May 2020").strftime('%Y-%m-%d'), flat_id: new_flat.id, user_id: new_user.id)
+
+# puts "Created Data. All good"
