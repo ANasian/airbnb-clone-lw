f5279b680d372af8bc8b97bb866cbb185c7034ca
Fabrizio Chiavari <fabrizio.chiavari@gmail.com>
2021-09-24 16:47:39 +0200

    changed indentation of create method in flats_controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -27,11 +27,11 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     @flat.user = current_user
-      if @flat.save
-        redirect_to flat_path(@flat)
-      else
-        render :new
-      end
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def show
